7dba686b890467f6dcce6f33ab9a3d1db2dec2b8
tensho.arai <tensho.arai@ities-inc.co.jp>
2024-04-05 18:39:55 +0900

    durk mode button fix

--- frontend/src/components/modalToggle.tsx
+++ frontend/src/components/modalToggle.tsx
@@ -13,28 +13,32 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-    const { setTheme } = useTheme()
+    const { theme, setTheme } = useTheme()
+
+    const changeTheme = () => {
+        theme === "dark" ? setTheme("light") : setTheme("dark")
+    }
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
+                <Button variant="outline" size="icon" onClick={changeTheme}>
                     <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                {/*<DropdownMenuItem onClick={() => setTheme("system")}>*/}
-                {/*    System*/}
-                {/*</DropdownMenuItem>*/}
-            </DropdownMenuContent>
+            {/*<DropdownMenuContent align="end">*/}
+            {/*    <DropdownMenuItem onClick={() => setTheme("light")}>*/}
+            {/*        Light*/}
+            {/*    </DropdownMenuItem>*/}
+            {/*    <DropdownMenuItem onClick={() => setTheme("dark")}>*/}
+            {/*        Dark*/}
+            {/*    </DropdownMenuItem>*/}
+            {/*    <DropdownMenuItem onClick={() => setTheme("system")}>*/}
+            {/*        System*/}
+            {/*    </DropdownMenuItem>*/}
+            {/*</DropdownMenuContent>*/}
         </DropdownMenu>
     )
 }
